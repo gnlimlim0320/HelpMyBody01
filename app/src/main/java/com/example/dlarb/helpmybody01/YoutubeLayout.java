bb96ba0a4ee498bdf9e2cc0a091f83015492fd1f
gnlimlim0320 <44458640+gnlimlim0320@users.noreply.github.com>
2018-10-31 13:02:23 +0900

    2018.10.31 (01) 리스트 뷰 구현 완료

--- app/src/main/java/com/example/dlarb/helpmybody01/YoutubeLayout.java
+++ app/src/main/java/com/example/dlarb/helpmybody01/YoutubeLayout.java
@@ -4,16 +4,39 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class YoutubeLayout extends Fragment {
     View v;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        v=inflater.inflate(R.layout.youtube_layout,container,false);
+        v = inflater.inflate(R.layout.youtube_layout, container, false);
+
+        String[] items = {"허리", "목"};
+        ListAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView) v.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                        String item = String.valueOf(parent.getItemAtPosition(i));
+
+                    }
+                }
+        );
         return v;
     }
 }
+
+
