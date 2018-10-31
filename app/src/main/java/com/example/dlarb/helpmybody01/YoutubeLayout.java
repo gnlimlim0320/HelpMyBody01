609badd0bbadf4eed36fa38fa66bc4cef141cd7d
gnlimlim0320 <44458640+gnlimlim0320@users.noreply.github.com>
2018-10-31 14:58:19 +0900

    2018.10.31 (02) 리스트뷰에서 intent 사용 -> 화면전환

--- app/src/main/java/com/example/dlarb/helpmybody01/YoutubeLayout.java
+++ app/src/main/java/com/example/dlarb/helpmybody01/YoutubeLayout.java
@@ -1,9 +1,11 @@
 package com.example.dlarb.helpmybody01;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -21,7 +23,7 @@ public class YoutubeLayout extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.youtube_layout, container, false);
 
-        String[] items = {"허리", "목"};
+        String[] items = {"허리","목","손목","팔","다리","전신"};
         ListAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
         ListView listView = (ListView) v.findViewById(R.id.listView);
         listView.setAdapter(adapter);
@@ -31,7 +33,8 @@ public class YoutubeLayout extends Fragment {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
                         String item = String.valueOf(parent.getItemAtPosition(i));
-
+                        Intent intent = new Intent(getActivity(), Neck.class);
+                        startActivity(intent);
                     }
                 }
         );
