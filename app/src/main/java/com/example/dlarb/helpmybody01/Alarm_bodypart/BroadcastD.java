9552d05421c7c7f27892ce738b11903ff8ae02f1
gnlimlim0320 <44458640+gnlimlim0320@users.noreply.github.com>
2018-11-02 23:43:22 +0900

    2018.11.02 (03) 입력받지 말고 리스트뷰로 반복 주기 선택 / 시도중...

--- app/src/main/java/com/example/dlarb/helpmybody01/Alarm_bodypart/BroadcastD.java
+++ app/src/main/java/com/example/dlarb/helpmybody01/Alarm_bodypart/BroadcastD.java
@@ -10,16 +10,18 @@ import android.content.Intent;
 import com.example.dlarb.helpmybody01.R;
 
 public class BroadcastD extends BroadcastReceiver {
-    String INTENT_ACTION=Intent.ACTION_BOOT_COMPLETED;
+    String INTENT_ACTION = Intent.ACTION_BOOT_COMPLETED;
+
     @Override
-    public void onReceive(Context context, Intent intent){
-        NotificationManager notificationManager=(NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context,0,new Intent(context,NeckAlarm.class),PendingIntent.FLAG_UPDATE_CURRENT);
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationmanager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, NeckAlarm.class), PendingIntent.FLAG_UPDATE_CURRENT);
         Notification.Builder builder = new Notification.Builder(context);
-        builder.setSmallIcon(R.drawable.ic_menu_gallery).setTicker("HETT").setWhen(System.currentTimeMillis())
+        builder.setSmallIcon(R.drawable.ic_menu_camera).setTicker("HETT").setWhen(System.currentTimeMillis())
                 .setNumber(1).setContentTitle("푸쉬 제목").setContentText("푸쉬내용")
                 .setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE).setContentIntent(pendingIntent).setAutoCancel(true);
 
-        notificationManager.notify(1,builder.getNotification());
+        notificationmanager.notify(1, builder.getNotification());
+
     }
 }
--- app/src/main/java/com/example/dlarb/helpmybody01/Alarm_bodypart/NeckAlarm.java
+++ app/src/main/java/com/example/dlarb/helpmybody01/Alarm_bodypart/NeckAlarm.java
@@ -1,43 +1,56 @@
 package com.example.dlarb.helpmybody01.Alarm_bodypart;
 
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import com.example.dlarb.helpmybody01.R;;import java.util.Calendar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import com.example.dlarb.helpmybody01.Alarm_bodypart.Alarm_min.none;
+import com.example.dlarb.helpmybody01.Alarm_bodypart.Alarm_min.onehour;
+import com.example.dlarb.helpmybody01.Alarm_bodypart.Alarm_min.thirty;
+import com.example.dlarb.helpmybody01.Alarm_bodypart.Alarm_min.twenty;
+import com.example.dlarb.helpmybody01.Alarm_bodypart.Alarm_min.twohour;
+import com.example.dlarb.helpmybody01.R;
+
 
 
 public class NeckAlarm extends AppCompatActivity {
-    private static int ONE_MINUTE = 6000;
-    @Override
-    protected void onCreate(Bundle bundle) {
+    @Nullable
+
+    protected void onCreate (Bundle bundle) {
         super.onCreate(bundle);
-        setContentView(R.layout.neck_layout);
+        setContentView(R.layout.alarm_neck);
         Intent intent = getIntent();
-        new AlarmNeck(getApplicationContext()).Alarm();
-        }
-    public class AlarmNeck{
-        private Context context;
-        public AlarmNeck(Context context){
-            this.context=context;
-        }
-        public void Alarm(){
-            AlarmManager am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
-            Intent intent = new Intent(NeckAlarm.this, BroadcastD.class);
 
-            PendingIntent sender = PendingIntent.getBroadcast(NeckAlarm.this,0,intent,0);
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTimeInMillis(System.currentTimeMillis());
-            calendar.set(Calendar.HOUR_OF_DAY,(int)System.currentTimeMillis());
-            calendar.set(Calendar.MINUTE,(int)System.currentTimeMillis());
-            calendar.set(Calendar.SECOND,(int)System.currentTimeMillis());
+        String[] minutes = {"20분", "30분", "1시간", "2시간", "취소"};
 
-            am.setRepeating(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),6000,sender);
-        }
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, minutes);
+        ListView listView = (ListView) findViewById(R.id.neck_alist);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    Class[] Activities = new Class[]{twenty.class, thirty.class, onehour.class, twohour.class, none.class};
+
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                        String item = String.valueOf(parent.getItemAtPosition(i));
+
+                        Intent intent = new Intent(NeckAlarm.this, Activities[i]);
+                        startActivity(intent);
+
+                    }
+                });
     }
-
 
 }
