34abab5bebf904d03dd6ca91adb7ea92d528cd0b
gnlimlim0320 <44458640+gnlimlim0320@users.noreply.github.com>
2018-11-11 02:40:29 +0900

    2018.11.11 (01) 이것저것 해보고 있는데 다 안됨ㅜㅜㅜㅜㅜㅜㅜ

--- app/src/main/java/com/example/dlarb/helpmybody01/YoutubeLayout.java
+++ app/src/main/java/com/example/dlarb/helpmybody01/YoutubeLayout.java
@@ -1,15 +1,20 @@
 package com.example.dlarb.helpmybody01;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -20,6 +25,7 @@ import com.example.dlarb.helpmybody01.bodypart.Neck;
 import com.example.dlarb.helpmybody01.bodypart.Waist;
 import com.example.dlarb.helpmybody01.bodypart.Wrist;
 
+
 public class YoutubeLayout extends Fragment {
     View v;
     @Nullable
@@ -27,12 +33,12 @@ public class YoutubeLayout extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.youtube_layout, container, false);
-
-        String[] items = {"허리","목","손목","팔","다리","전신"};
-
+        final String[] items = {"허리","목","손목","팔","다리","전신"};
         ListAdapter adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, items);
-        ListView listView = (ListView) v.findViewById(R.id.listView);
+        final ListView listView = (ListView) v.findViewById(R.id.listView);
         listView.setAdapter(adapter);
+
+
 
         listView.setOnItemClickListener(
                 new AdapterView.OnItemClickListener() {
@@ -40,14 +46,11 @@ public class YoutubeLayout extends Fragment {
 
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
-                        String item = String.valueOf(parent.getItemAtPosition(i));
-
                         Intent intent = new Intent(getActivity(), Activities[i]);
                         startActivity(intent);
-
-
                     }
                 }
+
         );
         return v;
     }
